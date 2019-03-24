ca755d12bb3fbbe87d70047eac6082ff712ef1ee
kulemin <kulemin86@bk.ru>
2019-03-24 23:30:09 +0400

    With static fields

--- Traversal.cs
+++ Traversal.cs
@@ -6,80 +6,51 @@ using System.Threading.Tasks;
 
 namespace Delegates.TreeTraversal
 {
-    public delegate object Search(object tree);
     public static class Traversal
     {
+        public static List<int> BinaryTreeResult = new List<int>();
+        public static List<Job> JobResult = new List<Job>();
+        public static List<Product> ProductResult = new List<Product>();
+
         public static List<int> GetBinaryTreeValues(BinaryTree<int> tree)
         {
-            var result = new List<int>();
             if (tree.Left != null)
             {
-                GetBinaryTreeValues(tree.Left, result);
+                GetBinaryTreeValues(tree.Left);
             }
             if (tree.Right != null)
             {
-                GetBinaryTreeValues(tree.Right, result);
+                GetBinaryTreeValues(tree.Right);
             }
-            result.Add(tree.Value);
-            return result;
-        }
-
-        public static List<int> GetBinaryTreeValues
-            (BinaryTree<int> tree, List<int> result)
-        {
-            if (tree.Left != null)
-            {
-                GetBinaryTreeValues(tree.Left, result);
-            }
-            if (tree.Right != null)
-            {
-                GetBinaryTreeValues(tree.Right, result);
-            }
-            result.Add(tree.Value);
-            return result;
+            BinaryTreeResult.Add(tree.Value);
+            return BinaryTreeResult;
         }
 
         public static List<Job> GetEndJobs(Job data)
         {
-            var result = new List<Job>();
-            if (data.Subjobs.Count() != 0) GetEndJobs(data.Subjobs, result);
-            else result.Add(data);
-            return result;
+            if (data.Subjobs.Count() != 0) GetEndJobs(data.Subjobs);
+            else JobResult.Add(data);
+            return JobResult;
         }
 
-        public static List<Job> GetEndJobs(List<Job> data, List<Job> result)
+        public static List<Job> GetEndJobs(List<Job> data)
         {
             foreach (var job in data)
             {
-                if (job.Subjobs.Count() != 0) GetEndJobs(job.Subjobs, result);
-                else result.Add(job);
+                if (job.Subjobs.Count() != 0) GetEndJobs(job.Subjobs);
+                else JobResult.Add(job);
             }
-            return result;
+            return JobResult;
         }
 
         public static List<Product> GetProducts(ProductCategory data)
         {
-            var result = new List<Product>();
             if (data.Categories.Count() != 0)
             {
-                foreach (var category in data.Categories)
-                    GetProducts(category, result);
+                foreach (var category in data.Categories) GetProducts(category);
             }
-            foreach (var product in data.Products)
-                result.Add(product);
-            return result;
-        }
-
-        public static List<Product> GetProducts(ProductCategory data, List<Product> result)
-        {
-            if (data.Categories.Count() != 0)
-            {
-                foreach (var category in data.Categories)
-                    GetProducts(category, result);
-            }
-            else foreach (var product in data.Products)
-                    result.Add(product);
-            return result;
+            foreach (var product in data.Products) ProductResult.Add(product);
+            return ProductResult;
         }
     }
 }
